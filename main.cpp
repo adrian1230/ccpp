124ae0ed3245783c9a83f57f96df79cd20840807
Chun Cheong So - Adrian <47051129+adrian1230@users.noreply.github.com>
2021-05-11 15:19:25 +0800

    omg

--- main.cpp
+++ main.cpp
@@ -10,29 +10,43 @@ class User {
         string Location;
         int Age;
 
+        // Behavior
         void introduction()
         {
             cout << "My name is " << Name << endl;
             cout << "I live in " << Location << endl;
             cout << "I am " << Age << " years old" << endl;
+        } 
+        
+        // Constructor
+        User(string name, string location, int age)
+        {
+            Name = name;
+            Location = location;
+            Age = age;
         }
 };
 
 int main()
 {
-    User user_one;
-    user_one.Age = 14;
-    user_one.Name = "Bain Fard";
-    user_one.Location = "Yorkshire";
+    User user_one = User(
+        "Bain Fard",
+        "Yorkshire",
+        14
+    );
+    // user_one.Name = "Bain Fard";
+    // user_one.Location = "Yorkshire";
+    // user_one.Age = 14;
     user_one.introduction();
 
-    User user_two;
-    user_two.Age = 34;
-    user_two.Name = "George Smither";
-    user_two.Location = "Manchester";
+    User user_two = User(
+        "George Smither",
+        "Manchester",
+        34
+    );
     user_two.introduction();
 
     // Default constructor
-    User user_default;
-    user_default.introduction();
+    // User user_default;
+    // user_default.introduction();
 };
